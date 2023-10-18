cc75bcfe28db035e79339f0658cab347ddae8a7f
João Palmeiro <joaommpalmeiro@gmail.com>
2023-10-18 22:49:24 +0100

    Closes #5

---
+++ src/categorical/FlatUI.ts
@@ -0,0 +1,14 @@
+// Source:
+// - https://github.com/proplot-dev/proplot/blob/v0.9.7/proplot/cycles/FlatUI.hex
+// - https://proplot.readthedocs.io/en/latest/cycles.html#Included-color-cycles
+
+const scheme: ReadonlyArray<string> = [
+  "#3498db",
+  "#e74c3c",
+  "#95a5a6",
+  "#34495e",
+  "#2ecc71",
+  "#9b59b6",
+];
+
+export default scheme;
--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ export { default as schemeCarbonLight14 } from "./categorical/CarbonLight14";
 export { default as schemeCategory20 } from "./categorical/Category20";
 export { default as schemeCategory20b } from "./categorical/Category20b";
 export { default as schemeCategory20c } from "./categorical/Category20c";
+export { default as schemeFlatUI } from "./categorical/FlatUI";
 export { default as schemeTableau20 } from "./categorical/Tableau20";
