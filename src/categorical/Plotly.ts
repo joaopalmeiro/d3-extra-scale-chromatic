e74aa2672aaeb8d057b22eadf6c2a6131c394b50
João Palmeiro <joaommpalmeiro@gmail.com>
2023-10-18 22:55:31 +0100

    Closes #6

---
+++ src/categorical/Plotly.ts
@@ -0,0 +1,19 @@
+// Source:
+// - https://github.com/plotly/plotly.py/blob/v5.17.0/packages/python/plotly/plotly/colors/__init__.py
+// - https://github.com/plotly/plotly.py/blob/v5.17.0/packages/python/plotly/_plotly_utils/colors/qualitative.py#L16
+// - https://plotly.com/python/discrete-color/#color-sequences-in-plotly-express
+
+const scheme: ReadonlyArray<string> = [
+  "#636efa",
+  "#ef553b",
+  "#00cc96",
+  "#ab63fa",
+  "#ffa15a",
+  "#19d3f3",
+  "#ff6692",
+  "#b6e880",
+  "#ff97ff",
+  "#fecb52",
+];
+
+export default scheme;
--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,5 @@ export { default as schemeCategory20 } from "./categorical/Category20";
 export { default as schemeCategory20b } from "./categorical/Category20b";
 export { default as schemeCategory20c } from "./categorical/Category20c";
 export { default as schemeFlatUI } from "./categorical/FlatUI";
+export { default as schemePlotly } from "./categorical/Plotly";
 export { default as schemeTableau20 } from "./categorical/Tableau20";
